100651fe6b1fc68dee7ba265f0c8426bf4d2ad15
kojiTrns <koji@desk>
2019-04-18 19:28:40 -0700

    Add env file for build on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python yt-data-crawler/src/dbtest.py
--- yt-data-crawler/src/dbtest.py
+++ yt-data-crawler/src/dbtest.py
@@ -1,7 +1,6 @@
 import os
 import psycopg2
 
-
 DATABASE_URL = os.environ['DATABASE_URL']
 
 def get_connection():
@@ -9,10 +8,10 @@ def get_connection():
 	return psycopg2.connect(DATABASE_URL, sslmode='require')
 
 with get_connection() as conn:
-    with conn.cursor() as cur:
-    	cur.execute('SELECT * FROM channels;')
-    	row = cur.fetchall()
-    	print row
-    	cur.execute("INSERT INTO  channels VALUES (%s, %s, %s, %s, %s)", ('UCd7US-V7-6KvL5uoIE4-Y_A', 'env', 12, 34, 56 ))
+	with conn.cursor() as cur:
+		cur.execute('SELECT * FROM channels;')
+		row = cur.fetchall()
+		print(row)
+		cur.execute("INSERT INTO  channels VALUES (%s, %s, %s, %s, %s)", ('UCd7US-V7-6KvL5uoIE4-Y_A', 'env', 12, 34, 56 ))
 	conn.commit()
 	
