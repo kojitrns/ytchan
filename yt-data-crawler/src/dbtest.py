e169bf01cb6539397a5781d6915af2b358e0c90f
kojiTrns <koji@desk>
2019-04-18 19:23:09 -0700

    db test for heroku

---
+++ yt-data-crawler/src/dbtest.py
@@ -0,0 +1,18 @@
+import os
+import psycopg2
+
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+def get_connection():
+	# dsn = os.environ.get('DATABASE_URL')
+	return psycopg2.connect(DATABASE_URL, sslmode='require')
+
+with get_connection() as conn:
+    with conn.cursor() as cur:
+    	cur.execute('SELECT * FROM channels;')
+    	row = cur.fetchall()
+    	print row
+    	cur.execute("INSERT INTO  channels VALUES (%s, %s, %s, %s, %s)", ('UCd7US-V7-6KvL5uoIE4-Y_A', 'env', 12, 34, 56 ))
+	conn.commit()
+	
