77f29adf0923bcc0d9a2e3134c3efcb962fa1373
koji_trns <koji@vm>
2019-04-27 04:26:29 +0900

    Update css of header

--- view/src/index.php
+++ view/src/index.php
@@ -13,10 +13,9 @@ function get_channel_cont($chan_data=null)
 {
 	global $youtube_url;
 	$chan_info = "<p>{$chan_data['viewcount']}</p><p>{$chan_data['subscribercount']}</p><p>{$chan_data['videocount']}</p>";
-
-	$cont = sprintf('<img src="%s"><div class="chan_info_box">%s</div><a href="%s%s" target=_blank title="%s"><div class ="chan_title_box">%s</div></a>', $chan_data['thumbnailurl'], $chan_info, $youtube_url, $chan_data['channelid'],
+	$cont = sprintf('<img src="%s"><div class="chan-info-box">%s</div><div class ="chan-title-box"><a href="%s%s" target=_blank title="%s">%s</a></div>', $chan_data['thumbnailurl'], $chan_info, $youtube_url, $chan_data['channelid'],
 		$chan_data['channeltitle'] ,$chan_data['channeltitle']);
-	return '<div class="chan_box">'. $cont .'</div>';
+	return '<div class="chan-box">'. $cont .'</div>';
 }
 
 function set_channel_database(&$category_list)
@@ -24,8 +23,8 @@ function set_channel_database(&$category_list)
 	try {
 	    // $pdo = new PDO('pgsql:host=localhost; dbname=d34ajosp96vv38', 'koji', 'mukku');
 		$dbopts = parse_url(getenv('DATABASE_URL'));
-		$pdo = new PDO( 'pgsql:host='.$dbopts["host"].'; dbname='.ltrim($dbopts["path"],'/'), $dbopts["user"], $dbopts["pass"] );
-		// var_dump("connection succeeded\n");
+		$pdo = new PDO('pgsql:host='.$dbopts["host"].'; dbname='.ltrim($dbopts["path"],'/'), $dbopts["user"], $dbopts["pass"]);
+		//var_dump("connection succeeded\n");
 		foreach($pdo->query('SELECT * from channel') as $row) {
 			$category_list[$row['maincategory']][$row['subcategory']][] = $row;
 	    }
@@ -37,9 +36,11 @@ function set_channel_database(&$category_list)
 }
 
 function show_header(&$category_list) {
+	echo '<header class="site-header"><nav><h4>カテゴリ</h4>';
 	foreach ($category_list as $maincategory => $sub_categories) {
 		echo "<a href=\"{$_SERVER["SCRIPT_NAME"]}?cur_category=$maincategory\">$maincategory</a> ";
 	}
+	echo '</nav></header><div class="header-emb"></div>';
 }
 
 // function init() {
@@ -50,16 +51,17 @@ $category_list = array();
 $cur_category = '';
 set_channel_database($category_list);
 show_header($category_list);
+
 if(isset($_GET['cur_category'])) {
 	$cur_category = $_GET['cur_category'];
 } else {
 	$cur_category = 'ニュース';
 }
 
-echo "<div class=\"main_category_panel\"><h2>$cur_category</h2>";
+echo "<div class=\"main-category-panel\"><h2>$cur_category</h2>";
 $sub_categories = $category_list[$cur_category];
 foreach ($sub_categories as $subcategory => $rows) {
-	echo "<div class=\"chan_container clearfix\"><h3>$subcategory</h3>";
+	echo "<div class=\"chan-container clearfix\"><h3>$subcategory</h3>";
 	foreach ($rows as $row) {
 		echo get_channel_cont($row);
 	}
