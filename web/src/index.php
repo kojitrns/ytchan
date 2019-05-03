ef11c80a231f89b1d0ca2a71d58733a7cb0c83d8
kojitrns <koji@vm>
2019-05-03 09:59:20 +0900

    Add social blade link
    And change entry dir from view to web

--- web/src/index.php
+++ web/src/index.php
@@ -13,14 +13,16 @@ function get_channel_cont($chan_data=null)
 {
 	global $youtube_url;
 	$description = '';
-	$chan_keywords = '';
+	$keywords = '';
 	if (strlen($chan_data['description'])) {
 		$description = "<span class=\"description\">説明<p>{$chan_data['description']}</p></span>";
 	}
 	if (strlen($chan_data['keywords'])) {
 		$keywords = "<span class=\"keywords\"> 関連語<p>{$chan_data['keywords']}</p></span>";
 	}
-	$chan_detail = "<div class=\"detail-box\">$description$keywords</div>";
+	$social_blade = "<a href=\"https://socialblade.com/youtube/channel/{$chan_data['channelid']}\" target=_blank> sbinfo</a>";
+
+	$chan_detail = "<div class=\"detail-box\">$description$keywords$social_blade</div>";
 
 	$chan_info = "<div class=\"counter-box\"><p>{$chan_data['viewcount']}</p><p>{$chan_data['subscribercount']}</p><p>{$chan_data['videocount']}</p></div>$chan_detail";
 	$cont = sprintf('<img src="%s"><div class="chan-info-box">%s</div><div class ="chan-title-box"><a href="%s%s" target=_blank title="%s">%s</a></div>', $chan_data['thumbnailurl'], $chan_info, $youtube_url, $chan_data['channelid'], $chan_data['channeltitle'] ,$chan_data['channeltitle']);
@@ -61,6 +63,14 @@ function show_left_panel(&$sub_categories) {
 	echo '</div>';
 }
 
+function sort_chan_rows(&$chan_rows, $sort_target) {
+	$sort;
+	foreach ($chan_rows as $num => $row) {
+		$sort[$num] = $row[$sort_target];
+	}
+	array_multisort($sort, SORT_DESC, $chan_rows);
+}
+
 // function init() {
 // }
 // $main_categories = file(__DIR__.'/main_category.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
@@ -81,8 +91,10 @@ show_left_panel(array_keys($category_list[$cur_category]));
 echo "<div class=\"main-category-panel\"><h2>$cur_category</h2>";
 
 $sub_categories = $category_list[$cur_category];
+
 foreach ($sub_categories as $subcategory => $rows) {
 	echo "<div class=\"chan-container clearfix\"><h3 id=\"$subcategory\">■$subcategory</h3>";
+	sort_chan_rows($rows, 'viewcount');
 	foreach ($rows as $row) {
 		echo get_channel_cont($row);
 	}
