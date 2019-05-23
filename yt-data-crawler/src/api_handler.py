b14a97afd5255c716c8c79a78e1a433f596c808a
kojitrns <koji@vm>
2019-05-23 20:25:50 +0900

    Retry api calling on channnel getting

--- yt-data-crawler/src/api_handler.py
+++ yt-data-crawler/src/api_handler.py
@@ -9,10 +9,17 @@ api_key = 'AIzaSyD3R2gavNlItHEZWTt-_UOMEwFwMN5reiQ'
 
 
 def get_channel_data(channel_id):
-	return requests.get(
+	chan_data = requests.get(
 	    reqPprefix+'channels?', params={'key': api_key,
 	    'part': 'statistics,snippet,brandingSettings,contentDetails',
-	    'id': channel_id}).json()['items'][0]
+	    'id': channel_id})
+	chan_data = error_check(chan_data)
+	if  chan_data is None:
+		print("retry! \n\n\n\n")
+		time.sleep(3)
+		return get_channel_data(channel_id)
+
+	return chan_data
 
 def get_latest_video_data(uplist_id):
 	videos = None
@@ -22,6 +29,7 @@ def get_latest_video_data(uplist_id):
 	    'playlistId': uplist_id})
 	videos = error_check(videos)
 	if  videos is None:
+		print("retry! \n\n\n\n")
 		time.sleep(3)
 		return get_channel_data(channel_id)
 
@@ -33,6 +41,7 @@ def get_latest_video_data(uplist_id):
 	video_data = error_check(video_data)
 
 	if video_data is None:
+		print("retry! \n\n\n\n")
 		time.sleep(3)
 		return get_channel_data(channel_id)
 
